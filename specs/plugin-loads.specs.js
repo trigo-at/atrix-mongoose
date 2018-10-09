499fca73ba0fcfed9054cc3ca6959b275337a7d7
Christian Hubinger <christian@trigo.at>
2018-10-09 13:42:23 +0200

    fix: logging fixe

--- specs/plugin-loads.specs.js
+++ specs/plugin-loads.specs.js
@@ -3,9 +3,10 @@
 /* eslint-env node, mocha */
 /* eslint no-unused-expressions: 0, arrow-body-style: 0 */
 
-const { expect } = require('chai');
-require('./service');
 const atrix = require('@trigo/atrix');
+const { expect } = require('chai');
+
+require('./service');
 
 describe('loads datasources into service', () => {
 	it('dataSources is populated with configs', async () => {
